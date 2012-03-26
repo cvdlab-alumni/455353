b5dd117585e8994f6fb37f5c240a9e54a5a656a1
Pierfrancesco <pierfrancisco89@hotmail.it>
2012-03-26 16:57:08 +0200

    prova

--- Domini.js
+++ Domini.js
@@ -21,4 +21,13 @@
 	}
 	var mapped = MAP(bisettrice)(domain)
 
-	//
+	// sinusoide
+
+	var domain = DOMAIN([[0,2*Math.PI]])([100]);
+	var mapping = function(p){
+		var u = p[0];
+		return [u, Math.sin(u)];
+	};       
+
+	var mapped = MAP(mapping)(domain)
+	DRAW(mapped);
