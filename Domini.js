1ec6f6e48d5de5fde0198d65348180bcd6abaa1d
Pierfrancesco <pierfrancisco89@hotmail.it>
2012-03-26 16:54:48 +0200

    prova

--- Domini.js
+++ Domini.js
@@ -4,7 +4,7 @@
 	var d2 = DOMAIN([[1.5,5.5],[1,3]])([4,2])
 	var d3 = DOMAIN([[1,5],[1,5],[1,5]])([3,3,3])
 
-
+	// tralazione
 	var domain = DOMAIN([[0,1]])([10]);
 	var mapping = function(p){
 		var u = p[0];
@@ -12,9 +12,13 @@
 	};       
 
 	var mapped = MAP(mapping)(domain)
+
+	// bisettrice
 	var bisettrice = function(p){
 		var u = p[0];
 		return [u,u];
 
 	}
 	var mapped = MAP(bisettrice)(domain)
+
+	//
